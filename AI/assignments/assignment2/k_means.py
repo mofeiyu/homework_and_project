765e740ed824c6110f3d9af89b5d505ba8d61b5e
rico sun <ricosr@163.com>
2018-11-03 17:00:53 +0800

    K-means and K-modes done

--- AI/assignments/assignment2/k_means.py
+++ AI/assignments/assignment2/k_means.py
@@ -3,8 +3,12 @@
 # Name: SUN RUI    ID:18083229g
 
 import pandas as pd
+import numpy as np
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def load_data(file_path):
@@ -16,12 +20,86 @@ def load_data(file_path):
 
 
 def do_k_means(data):
-    k_means = KMeans(n_clusters=3, random_state=0)
-    print(k_means.fit_predict(data))
-    print(k_means.fit(data).cluster_centers_)
+    centers = np.vstack((data[0], data[7], data[14]))
+    k_means = KMeans(n_clusters=3, init=centers, n_init=1, max_iter=1000)
+    return k_means.fit_predict(data), k_means.fit(data).cluster_centers_
 
+def plot_distribution_2D(index, data):
+    cluster0 = []
+    cluster1 = []
+    cluster2 = []
+    for i in range(len(cluster_index)):
+        if cluster_index[i] == 0:
+            cluster0.append(data[i])
+        if cluster_index[i] == 1:
+            cluster1.append(data[i])
+        if cluster_index[i] == 2:
+            cluster2.append(data[i])
+    pca = PCA(2)
+    color_ls = ["red", "green", "blue"]
+    all_clusters = [cluster0, cluster1, cluster2]
+    for i in range(len(all_clusters)):
+        plot_columns = pca.fit_transform(all_clusters[i])
+        X = plot_columns[:, 0]
+        Y = plot_columns[:, 1]
+        plt.scatter(X, Y, s=200, c=color_ls[i], alpha=.5)
+    plt.xlim(-3, 3)
+    plt.ylim(-3, 3)
+    plt.show()
+
+def plot_distribution_3D(cluster_index, data):
+    cluster0 = []
+    cluster1 = []
+    cluster2 = []
+    for i in range(len(cluster_index)):
+        if cluster_index[i] == 0:
+            cluster0.append(data[i])
+        if cluster_index[i] == 1:
+            cluster1.append(data[i])
+        if cluster_index[i] == 2:
+            cluster2.append(data[i])
+    pca = PCA(3)
+    color_ls = ["red", "green", "blue"]
+    all_clusters = [cluster0, cluster1, cluster2]
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    for i in range(len(all_clusters)):
+        plot_columns = pca.fit_transform(all_clusters[i])
+        X = plot_columns[:, 0]
+        Y = plot_columns[:, 1]
+        Z = plot_columns[:, 2]
+        ax.scatter(X, Y, Z, s=200, c=color_ls[i], alpha=.5)
+    ax.set_zlabel('Z')
+    ax.set_ylabel('Y')
+    ax.set_xlabel('X')
+    plt.show()
 
 data = load_data("data.csv")
-do_k_means(data)
+cluster_index, cluster_center = do_k_means(data)
+plot_distribution_2D(cluster_index, data)
+plot_distribution_3D(cluster_index, data)
+print(cluster_index)
+# print(cluster_center)
 
 
+#
+# cluster0 = []
+# cluster1 = []
+# cluster2 = []
+# for i in range(len(cluster_index)):
+#     if cluster_index[i] == 0:
+#         cluster0.append(data[i])
+#     if cluster_index[i] == 1:
+#         cluster1.append(data[i])
+#     if cluster_index[i] == 2:
+#         cluster2.append(data[i])
+# pca = PCA(3)
+# plot_columns = pca.fit_transform(cluster0)
+# fig = plt.figure()
+# ax = Axes3D(fig)
+# X = plot_columns[:, 0]
+# Y = plot_columns[:, 1]
+# Z = plot_columns[:, 2]
+# ax.scatter(X, Y, Z, s=100)
+#
+# plt.show()
--- AI/assignments/assignment2/k_modes.py
+++ AI/assignments/assignment2/k_modes.py
@@ -3,20 +3,74 @@
 # Name: SUN RUI    ID:18083229g
 
 import pandas as pd
+import numpy as np
 from kmodes.kmodes import KModes
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
 
 def load_data(file_path):
     df_data = pd.read_csv(file_path)
     df_data.columns = ["Age", "Sex", "MonthlyIncome", "MaritalStatus",	"ServicePlan", "ExtraUsage"]
-    # print(df_data)
-    # scaled_data = preprocessing.scale(df_data)
-    # return scaled_data
-# random categorical data
-# data = np.random.choice(20, (100, 10))
-# print(data)
-km = KModes(n_clusters=4, init='Huang', n_init=5, verbose=1)
+    return df_data
 
-clusters = km.fit_predict(data)
+def do_k_modes(data):
+    one_hot_data = np.array(pd.get_dummies(df_data))
+    centroids = np.vstack((one_hot_data[0], one_hot_data[7], one_hot_data[14]))
+    k_modes = KModes(n_clusters=3, init=centroids, n_init=1)
+    return k_modes.fit_predict(one_hot_data), one_hot_data
 
-# Print the cluster centroids
-print(km.cluster_centroids_)
+def plot_distribution_2D(index, data):
+    cluster0 = []
+    cluster1 = []
+    cluster2 = []
+    for i in range(len(cluster_index)):
+        if cluster_index[i] == 0:
+            cluster0.append(data[i])
+        if cluster_index[i] == 1:
+            cluster1.append(data[i])
+        if cluster_index[i] == 2:
+            cluster2.append(data[i])
+    pca = PCA(2)
+    color_ls = ["red", "green", "blue"]
+    all_clusters = [cluster0, cluster1, cluster2]
+    for i in range(len(all_clusters)):
+        plot_columns = pca.fit_transform(all_clusters[i])
+        X = plot_columns[:, 0]
+        Y = plot_columns[:, 1]
+        plt.scatter(X, Y, s=200, c=color_ls[i], alpha=.5)
+    plt.xlim(-3, 3)
+    plt.ylim(-3, 3)
+    plt.show()
+
+def plot_distribution_3D(cluster_index, data):
+    cluster0 = []
+    cluster1 = []
+    cluster2 = []
+    for i in range(len(cluster_index)):
+        if cluster_index[i] == 0:
+            cluster0.append(data[i])
+        if cluster_index[i] == 1:
+            cluster1.append(data[i])
+        if cluster_index[i] == 2:
+            cluster2.append(data[i])
+    pca = PCA(3)
+    color_ls = ["red", "green", "blue"]
+    all_clusters = [cluster0, cluster1, cluster2]
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    for i in range(len(all_clusters)):
+        plot_columns = pca.fit_transform(all_clusters[i])
+        X = plot_columns[:, 0]
+        Y = plot_columns[:, 1]
+        Z = plot_columns[:, 2]
+        ax.scatter(X, Y, Z, s=200, c=color_ls[i], alpha=.5)
+
+    plt.show()
+
+
+df_data = load_data("data_depth.csv")
+cluster_index, one_hot_data = do_k_modes(df_data)
+plot_distribution_2D(cluster_index, one_hot_data)
+plot_distribution_3D(cluster_index, one_hot_data)
+print(cluster_index)
