01b8a12ae669b729d177966ce256a6d7f4184518
rico sun <ricosr@163.com>
2018-11-05 15:18:52 +0800

    betweenness computing done

--- AI/assignments/assignment2/betweenness.py
+++ AI/assignments/assignment2/betweenness.py
@@ -1,4 +1,7 @@
+import eji_data
 import eji_data2
+import eji_data3
+
 # 1
 # A_ls = [1,1,1,1,1,1,1,2,3]
 # B_ls = [1,1,1,1,3,1,1,2]
@@ -11,9 +14,20 @@ import eji_data2
 # J_ls = [1]
 
 # 2
+# A_ls = [1,1,1,1,1,1,1,1,2]
+# B_ls = [1,1,1,1,2,1,1,2]
+# C_ls = [1,1,2,1,2,1,2]
+# D_ls = [1,1,1,1,1,2]
+# E_ls = [1,1,1,1,2]
+# F_ls = [1,1,1,2]
+# G_ls = [1,1,2]
+# H_ls = [1,1]
+# J_ls = [1]
+
+# 3
 A_ls = [1,1,1,1,1,1,1,1,2]
-B_ls = [1,1,1,1,2,1,1,2]
-C_ls = [1,1,2,1,2,1,2]
+B_ls = [1,1,1,1,1,1,1,2]
+C_ls = [1,1,1,1,1,1,2]
 D_ls = [1,1,1,1,1,2]
 E_ls = [1,1,1,1,2]
 F_ls = [1,1,1,2]
@@ -21,7 +35,7 @@ G_ls = [1,1,2]
 H_ls = [1,1]
 J_ls = [1]
 all_ls = A_ls + B_ls + C_ls + D_ls + E_ls + F_ls + G_ls + H_ls + J_ls
-AB_ls = eji_data2.EF
+AB_ls = eji_data3.JK
 
 betweenness = 0
 for (up, down) in zip(AB_ls, all_ls):
